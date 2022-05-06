614a85b4356447e3e5a9d3642f2011837b3ef8ce
rmnsym <103978146+rmnsym@users.noreply.github.com>
2022-05-06 10:40:35 +0900

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(
+    name = "bb",
+    author = "Ruman Suyama",
+    version = "0.1.0",
+    about = "野球の打者指標を算出する"
+)]
+struct Options {
+}
+
+fn main() {
+}
